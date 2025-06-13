edd91c2a17d7b4c6c1e780943e3d89798e1c9b64
black <black@GIDDY_CRIMSON>
2025-06-13 03:57:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Student Name
+// CIS 261
+// Lab 1: Guessing Game
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
